b1c18b075e1320db4205eb9ccdd727ac438c4bcc
ngzhenghui94 <ngzhenghui94@hotmail.com>
2023-06-02 10:34:30 +0000

    updated and revoked huggingface api token

--- src/stableDiffusion.js
+++ src/stableDiffusion.js
@@ -1,10 +1,15 @@
 
 import axios from "axios"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let huggingFaceToken = process.env.HUGGINGFACEKEY;
 
 export const queryStableDiffusion = async (data) => {
     console.log(data)
     const response = await fetch("https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2-1-base", {
-        headers: { Authorization: "Bearer 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" },
+        headers: { Authorization: `Bearer ${huggingFaceToken}` },
         method: "POST",
         body: JSON.stringify(data),
     })
