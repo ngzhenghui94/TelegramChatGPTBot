286205e83d7ea43349aa13a5e3bda43f2c82faa9
Daniel Ng Zheng Hui <ngzhenghui94@hotmail.com>
2023-06-14 02:33:50 +0000

    MongoDb fix

--- src/mongodb.js
+++ src/mongodb.js
@@ -14,13 +14,13 @@ export const mongoClient = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await mongoClient.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await mongoClient.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoClient.close();
   }
 }
 
